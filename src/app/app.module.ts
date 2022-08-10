4587327e32f67d97bb2e3f55621fa60787bcd0eb
raju-tagline <raju.tagline@gmail.com>
2022-08-10 16:57:38 +0530

    Intergrates video game API and get data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnetNowComponent } from './connet-now/connet-now.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConnetNowComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ConnetNowComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/connet-now/connet-now.component.ts
+++ src/app/connet-now/connet-now.component.ts
@@ -1,15 +1,37 @@
+import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface IResponse {
+  first_release_date: number;
+  id: number;
+  name: string;
+  rating: number;
+  summary: string;
+}
 
 @Component({
   selector: 'app-connet-now',
   templateUrl: './connet-now.component.html',
-  styleUrls: ['./connet-now.component.scss']
+  styleUrls: ['./connet-now.component.scss'],
 })
 export class ConnetNowComponent implements OnInit {
+  private endPoint: string = environment.URL;
+  public videoGameList!: IResponse[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    this.getListOfGames();
   }
 
+  ngOnInit(): void {}
+
+  getListOfGames() {
+    this.http
+      .get<IResponse[]>(`${this.endPoint}`)
+      .subscribe((resp: IResponse[]) => {
+        if (resp?.length) {
+          this.videoGameList = resp;
+        }
+      });
+  }
 }
