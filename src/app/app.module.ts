5427056ef39f66a4eb1c346c66a43588d11eb8c3
raju-tagline <raju.tagline@gmail.com>
2022-08-12 09:53:27 +0530

    Create form and find the game from the list of array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { ConnetNowComponent } from './connet-now/connet-now.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ConnetNowComponent, ContactComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/connet-now/connet-now.component.ts
+++ src/app/connet-now/connet-now.component.ts
@@ -1,9 +1,11 @@
 import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as moment from 'moment';
 
 export interface IResponse {
-  first_release_date: number;
+  first_release_date: Date;
   id: number;
   name: string;
   rating: number;
@@ -18,20 +20,51 @@ export interface IResponse {
 export class ConnetNowComponent implements OnInit {
   private endPoint: string = environment.URL;
   public videoGameList!: IResponse[];
+  public isDropDownShow: boolean = false;
+  public videoGameFilterForm!: FormGroup;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
     this.getListOfGames();
+    this.createForm();
   }
 
-  ngOnInit(): void {}
+  createForm() {
+    this.videoGameFilterForm = new FormGroup({
+      name: new FormControl(''),
+      score: new FormControl(''),
+      order: new FormControl(''),
+    });
+  }
 
   getListOfGames() {
     this.http
       .get<IResponse[]>(`${this.endPoint}`)
       .subscribe((resp: IResponse[]) => {
         if (resp?.length) {
-          this.videoGameList = resp;
+          this.videoGameList = resp.map((res: any) => {
+            return {
+              ...res,
+              first_release_date: moment(
+                new Date(res.first_release_date)
+              ).format('MM/DD/YYYY'),
+            };
+          });
         }
       });
+  }
+
+  findValue(event: any) {
+    if (event?.target?.value && event?.target?.value.length) {
+      const videoGames: IResponse[] = this.videoGameList;
+      const listOfGame: IResponse[] = [];
+      videoGames.find((ele: IResponse) => {
+        if (ele && ele.name.toLowerCase().includes(event.target.value)) {
+          listOfGame.push(ele);
+        }
+      });
+      this.videoGameList = listOfGame;
+    }
   }
 }
