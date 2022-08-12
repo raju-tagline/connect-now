bbcde28793b9b8e114642f488ef725fe02fd035f
raju-tagline <raju.tagline@gmail.com>
2022-08-12 15:19:34 +0530

    Create form for contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  public contactFrom!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.createFrom();
   }
 
+  createFrom() {
+    this.contactFrom = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+    });
+  }
+
+  submit() {
+    if (this.contactFrom.invalid) {
+      return;
+    }
+    this.contactFrom.reset();
+  }
 }
